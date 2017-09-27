c3ff622163e537b21b67146185231cac5795ac40
paul wisehart <paul@oldcode.org>
2017-09-27 18:45:05 -0400

    asd

--- lg.go
+++ lg.go
@@ -12,13 +12,11 @@ var Log *log.Logger
 
 func init() {
 	log_file := config.Get("log_file")
-	//fmt.Printf("log_file:%s\n", log_file)
 	f, err := os.OpenFile(log_file, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
-		panic(fmt.Sprintf("error opening file:%s"))
+		panic(fmt.Sprintf("error opening file:%s err:%s", log_file, err.Error()))
 	}
 
-	//Log = log.New(f, "logger: ", log.Llongfile)
 	Log = log.New(f, ":", log.Lshortfile)
-	//log.SetOutput(f)
+	Log.Println("opening logfile ...")
 }
